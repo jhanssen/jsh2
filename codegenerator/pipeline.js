c020007bfe0f4bff6d9e947e22b515452f8fd93b
Jan Erik Hanssen <jhanssen@gmail.com>
2017-02-20 22:02:02 -0800

    pipeline

--- codegenerator/pipeline.js
+++ codegenerator/pipeline.js
@@ -1,3 +1,12 @@
 /*global require,module*/
 
-module.exports = function() { return ""; };
+module.exports = function(ast) {
+    var out = this._inc("{\n");
+    out += this._i("jsh.commands.push(new jsh.CommandPipeline());\n");
+    for (var i = 0; i < ast.commands.length; ++i) {
+        out += this._generate(ast.commands[i]);
+    }
+    out += this._i("jsh.commands.pop();\n");
+    out += this._dec("}\n");
+    return out;
+};
