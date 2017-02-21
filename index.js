cc48bef1f2b8f1f072704bfdef02bc1a7704fc32
Jan Erik Hanssen <jhanssen@gmail.com>
2017-02-21 01:54:14 -0800

    more

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 /*global require*/
 
-const console = require("./lib/console");
-const commands = require("./lib/commands");
-const builtins = require("./lib/builtins");
+"use strict";
 
-commands.init();
-builtins.init();
-console.run();
+const JSH = require("./lib/jsh/index");
+const jsh = new JSH();
+//console.log(jsh.commands);
+
+{
+
+    const console = require("./lib/console");
+    const commands = require("./lib/commands");
+    const builtins = require("./lib/builtins");
+
+    commands.init();
+    builtins.init();
+    console.run(jsh);
+}
