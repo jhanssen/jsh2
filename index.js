f0fceab2ef8dc6526de17936f7c44164546218a9
Jan Erik Hanssen <jhanssen@gmail.com>
2017-03-02 00:56:48 -0800

    work

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 "use strict";
 
+process.on('uncaughtException', (err) => {
+    console.error("uncaught exception", err);
+});
+
 const nativeJsh = require("native-jsh");
 const native = nativeJsh.init();
 
@@ -20,7 +24,7 @@ function loadrc()
         };
         rc(jsh);
     } catch (e) {
-        console.log("no rc file");
+        console.log("no rc file", e);
     }
 }
 
@@ -28,18 +32,17 @@ const CodeRunner = require("./lib/coderunner/index");
 const runner = new CodeRunner();
 //console.log(jsh.commands);
 
-{
-    const console = require("./lib/console");
-    const commands = require("./lib/commands");
-    const builtins = require("./lib/builtins");
+const console = require("./lib/console");
+const commands = require("./lib/commands");
+const builtins = require("./lib/builtins");
 
-    commands.init();
-    builtins.init();
-    if (native.interactive) {
-        console.run(runner);
+commands.init();
+builtins.init();
 
-        loadrc();
-    } else {
-        // we'll want to read arguments at this point and execute commands
-    }
+if (native.interactive) {
+    console.run(runner);
+
+    loadrc();
+} else {
+    // we'll want to read arguments at this point and execute commands
 }
